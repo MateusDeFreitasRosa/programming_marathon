ee2aa1c210488bd3e3fcb5f0697117e7efde918f
MateusDeFreitasRosa <45501846+MateusDeFreitasRosa@users.noreply.github.com>
2019-11-01 18:18:30 -0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string encrypt(string phrase, int key) {
+    for(int i=0; i<phrase.size() ; i++) {
+        if (isalpha(phrase[i]))
+            phrase[i] = (((phrase[i]-65)+key)%26)+65;
+    }
+    return phrase;
+}
+
+int main()
+{
+    string phrase, r;
+    int key;
+
+    getline(cin, phrase);
+    cin >> key;
+
+    cout << encrypt(phrase, key) << endl;
+    return 0;
+}
